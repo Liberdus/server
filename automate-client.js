663b06f5bfdbf226a779fc309a9299787adae857
Aamir Syed <asyed94@gmail.com>
2020-03-23 19:47:29 -0500

    Adds client automation code

---
+++ automate-client.js
@@ -0,0 +1,43 @@
+const { spawn } = require('child_process')
+const { promisify } = require('util')
+const sleep = promisify(setTimeout)
+
+async function main () {
+  const client = spawn('node', ['./client-setTxFee.js'], { stdio: ['pipe', 'inherit', 'inherit'] })
+
+  await sleep(1000)
+
+  client.stdin.write('aamir\n')
+  await sleep(500)
+
+  /*
+  client.stdin.write('register\n')
+  await sleep(500)
+  client.stdin.write('aamir\n')
+  await sleep(5000)
+
+  client.stdin.write('use omar\n')
+  await sleep(500)
+  client.stdin.write('register\n')
+  await sleep(500)
+  client.stdin.write('omar\n')
+  await sleep(5000)
+
+  client.stdin.write('query aamir\n')
+  await sleep(2000)
+  client.stdin.write('query omar\n')
+  await sleep(2000)
+  */
+
+  client.stdin.write('setTxFee\n')
+  await sleep(500)
+  client.stdin.write('\n')
+  await sleep(500)
+
+  client.stdin.write('exit\n')
+  await sleep(500)
+
+  // client.kill('SIGKILL')
+}
+
+main()
