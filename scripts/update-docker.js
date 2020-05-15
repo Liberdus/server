0f2e63194a8672e2242c368292fc9ae0918a488c
Aamir Syed <asyed94@gmail.com>
2020-05-15 14:44:41 -0500

    Makes update-docker script checks work as intended

--- scripts/update-docker.js
+++ scripts/update-docker.js
@@ -1,6 +1,6 @@
 const execa = require('execa')
 const { readFileSync } = require('fs')
-const { join } = require('path')
+const { join, parse } = require('path')
 
 // Set the resulting docker image's tag and Dockerfile based on passed arg
 const tag = process.argv[2]
@@ -34,14 +34,34 @@ if (packageJson.dependencies['shardus-global-server']) {
   process.exit(1)
 }
 
-try {
-  const distPath = require.resolve('shardus-global-server-dist')
-} catch (err) {
-  console.error('Error: "shardus-global-server-dist" is not in package.json')
+if (!packageJson.dependencies['shardus-global-server-dist']) {
+  console.error('Error: "shardus-global-server-dist" is not in in package.json')
   console.error()
   process.exit(1)
 }
-const distPackageJson = JSON.parse(readFileSync(join(distPath, 'package.json')))
+
+let distDir
+try {
+  distDir = parse(require.resolve('shardus-global-server-dist/package.json')).dir
+} catch (err) {
+  console.error(err)
+  console.error()
+  console.error('Error: "shardus-global-server-dist" is not in node_modules')
+  console.error('Try running npm install')
+  console.error()
+  process.exit(1)
+}
+
+let distPackageJson
+try {
+  distPackageJson = JSON.parse(readFileSync(join(distDir, 'package.json')))
+} catch (err) {
+  console.error(err)
+  console.error()
+  console.error('Error: Could not open "shardus-global-server-dist" package.json')
+  console.error()
+  process.exit(1)
+}
 
 if (distPackageJson.name !== 'shardus-global-server-dist') {
   console.error('Error: "shardus-global-server-dist" has been linked to something else')
@@ -50,5 +70,7 @@ if (distPackageJson.name !== 'shardus-global-server-dist') {
 }
 
 // Build the docker image and push it to the gitlab registry
+/*
 execa.commandSync(`docker build -t registry.gitlab.com/liberdus/server:${tag} -f ${dockerfile} .`, { stdio: [0, 1, 2] })
 execa.commandSync(`docker push registry.gitlab.com/liberdus/server:${tag}`, { stdio: [0, 1, 2] })
+*/
