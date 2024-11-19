276dd81024f0ed46f35c1115bc5f05af54e0bd1e
Kaung Myat Thu <2962000kaungmyatthu@gmail.com>
2024-11-19 16:05:18 +0700

    feat address conversion fallback

--- src/utils/address.ts
+++ src/utils/address.ts
@@ -1,6 +1,12 @@
 import {LiberdusFlags} from '../config'
 import {UserAccount} from '../@types'
 
+/**
+  * Converts an Ethereum address to a Shardus address by padding to fill 64 bytes.
+  * Will strip the '0x' prefix if present.
+  * @param addressStr - The Ethereum address to convert.
+  * @returns The Shardus address.
+**/
 export function toShardusAddress(addressStr: string): string {
   if (LiberdusFlags.VerboseLogs) {
     console.log(`Running toShardusAddress`, typeof addressStr, addressStr)
@@ -8,5 +14,16 @@ export function toShardusAddress(addressStr: string): string {
 
   //change this:0x665eab3be2472e83e3100b4233952a16eed20c76
   //    to this:  665eab3be2472e83e3100b4233952a16eed20c76000000000000000000000000
-  return addressStr.slice(2).toLowerCase() + '0'.repeat(24)
+
+  let byte64Str = addressStr
+
+  if (addressStr.startsWith('0x')) {
+    byte64Str = addressStr.slice(2)
+  }
+
+  if (byte64Str.length === 64) {
+    return byte64Str
+  }
+  
+  return byte64Str + '0'.repeat(64 - byte64Str.length)
 }
