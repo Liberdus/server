66fe924319ab2d46b78de87c58f5f1c3f2614912
Thant Sin Toe <mr.thantsintoe@gmail.com>
2025-02-25 11:30:21 +0700

    fix migrate function to correctly update the configs

--- src/versioning/migrations/2.3.5.ts
+++ src/versioning/migrations/2.3.5.ts
@@ -1,6 +1,6 @@
 import { nestedCountersInstance } from '@shardeum-foundation/core'
 import { Migration } from '../types'
-import config, { LiberdusFlags } from '../../config'
+import { shardusConfig } from '../..'
 
 // This has been baked into settings and is not needed, but the goal is to keep one migration as
 // an example for when we need to migrate again.
@@ -9,9 +9,9 @@ export const migrate: Migration = async () => {
   console.log('migrate 2.3.5')
   nestedCountersInstance.countEvent('migrate', 'calling migrate 2.3.5')
 
-  config.server.stateManager.removeStuckTxsFromQueue = false
-  config.server.stateManager.removeStuckTxsFromQueue2 = false
-  config.server.stateManager.removeStuckTxsFromQueue3 = false
+  shardusConfig.stateManager.removeStuckTxsFromQueue = false
+  shardusConfig.stateManager.removeStuckTxsFromQueue2 = false
+  shardusConfig.stateManager.removeStuckTxsFromQueue3 = false
 }
 
 //WARNING if you add a new one of these migration files you must add it to the migrations list in
