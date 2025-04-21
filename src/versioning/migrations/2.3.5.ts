9367965bada609399266d15408012acefaa43f9f
Thant Sin Toe <mr.thantsintoe@gmail.com>
2025-04-21 10:49:21 +0700

    enable AJV validation in version 2.3.5

--- src/versioning/migrations/2.3.5.ts
+++ src/versioning/migrations/2.3.5.ts
@@ -1,5 +1,6 @@
 import { nestedCountersInstance } from '@shardeum-foundation/core'
 import { Migration } from '../types'
+import { LiberdusFlags } from '../../config'
 import { shardusConfig } from '../..'
 
 // This has been baked into settings and is not needed, but the goal is to keep one migration as
@@ -9,9 +10,7 @@ export const migrate: Migration = async () => {
   console.log('migrate 2.3.5')
   nestedCountersInstance.countEvent('migrate', 'calling migrate 2.3.5')
 
-  shardusConfig.stateManager.removeStuckTxsFromQueue = false
-  shardusConfig.stateManager.removeStuckTxsFromQueue2 = false
-  shardusConfig.stateManager.removeStuckTxsFromQueue3 = false
+  LiberdusFlags.enableAJVValidation = true
 }
 
 //WARNING if you add a new one of these migration files you must add it to the migrations list in
