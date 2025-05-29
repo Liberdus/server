c5c20ad055f0fef4233bd42fd4ae5c348d37b8e0
jairajdev <jairajranamagar.dev@gmail.com>
2025-05-29 15:03:35 +0545

    Expose the messages/:chatId/toll endpoint

--- src/api/messages/index.ts
+++ src/api/messages/index.ts
@@ -1,31 +1,7 @@
-import { ChatAccount } from '../../@types'
+import { messages } from './messages'
+import { toll } from './toll'
 
-/**
- * an API like:
-  /messages/:chatId/:timestamp which returns only the message that 
-  have a timestamp >= the given timestamp. 
-  Returns all message if a timestamp is not provided. 
- */
-
-export default (dapp) =>
-  async (req, res): Promise<void> => {
-    try {
-      const chatId = req.params['chatId']
-      const timestampStr = req.params['timestamp']
-      const timestamp = Number(timestampStr) || 0
-      const account = await dapp.getLocalOrRemoteAccount(chatId)
-      if (!account) {
-        res.json({ error: "Chat doesn't exist" })
-        return
-      }
-      if (!account.data.messages) {
-        res.json({ error: 'no chat history for this request' })
-      } else {
-        const messages = account.data.messages.filter((msg) => msg.timestamp >= timestamp)
-        res.json({ messages })
-      }
-    } catch (error) {
-      dapp.log(error)
-      res.json({ error })
-    }
-  }
+export default {
+  messages,
+  toll,
+}
