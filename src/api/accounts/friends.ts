04f7dff6992491564d277e06c86ae451f437c93d
Abdul Azeem <sabdulazeem01@gmail.com>
2026-01-12 21:31:08 +0300

    feat(api): Implement deprecation check for GET /account/:id/friends

--- src/api/accounts/friends.ts
+++ src/api/accounts/friends.ts
@@ -1,5 +1,22 @@
+import * as AccountsStorage from '../../storage/accountStorage'
+import * as utils from '../../utils'
+
 export const friends = dapp => async (req, res): Promise<void> => {
   try {
+    // Deprecation check - reject when network version >= 2.5.0
+    // This check is disabled until network version is flipped to 2.5.0 or higher
+    if (
+      AccountsStorage?.cachedNetworkAccount &&
+      utils.isEqualOrNewerVersion('2.5.0', AccountsStorage.cachedNetworkAccount.current.activeVersion)
+    ) {
+      res.status(410).json({
+        error: 'This endpoint is deprecated and no longer available',
+        deprecated: true,
+        deprecatedVersion: '2.5.0',
+      })
+      return
+    }
+
     const id = req.params['id']
     const account = await dapp.getLocalOrRemoteAccount(id)
     if (account) {
